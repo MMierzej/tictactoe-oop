eb89b6a7d6b453861035027e0504401611337bd9
Marcin Mierzejewski <322680@uwr.edu.pl>
2021-06-25 20:37:26 +0200

    User works.

--- src/player/User.java
+++ src/player/User.java
@@ -1,5 +1,7 @@
 package player;
 
+import java.util.Scanner;
+
 import board.Board;
 
 /**
@@ -7,17 +9,43 @@ import board.Board;
  */
 public class User extends Player {
     /**
+     * Pozwala pobrać wejście od użytkownika.
+     */
+    Scanner scan;
+
+    /**
      * Konstruktor obiektu gracza-użytkownika.
      * @param c znak gracza.
      */
     public User(char c) {
         super(c);
+        scan = new Scanner(System.in);
     }
 
     @Override
     public int[] chooseCoordinates(Board board) {
         int[] coords = new int[2];
+        String[] input;
 
-        return coords;
+        while (true) {
+            System.out.print("Enter row and column: ");
+
+            try {
+                input = scan.nextLine().trim().split("\\s+");
+                coords[0] = Integer.parseInt(input[0]) - 1;
+                coords[1] = Integer.parseInt(input[1]) - 1;
+            } catch (Exception e) {
+                System.out.println("You should enter numbers!");
+                continue;
+            }
+
+            if (coords[1] < 0 || coords[1] > 2 || coords[0] < 0 || coords[0] > 2) {
+                System.out.println("Coordinates should be from 1 to 3!");
+                // continue;
+            } else if (board.isFree(coords[0], coords[1])) {
+                return coords;
+            } else 
+                System.out.println("This cell is occupied! Choose another one.");
+        }
     }
 }
